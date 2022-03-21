c6e763ac88d6115a97cebf745fd5b44e97f27b71
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-21 17:47:03 +0200

    added launch and testitem request models

---
+++ Models/Api/FinishLaunchRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ReportPortalNUnitLog4netClient.Models.Api
+{
+    public class FinishLaunchRequest
+    {
+        [JsonProperty("endTime")]
+        public DateTime EndTime { get; set; }
+        [JsonProperty("attributes")]
+        public List<Attribute> Attributes { get; set; }
+    }
+}
