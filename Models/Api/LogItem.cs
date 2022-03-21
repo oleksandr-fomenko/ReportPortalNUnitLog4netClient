f1fb01e244266b77b4a508f0910e6b77ae27e53c
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-21 17:43:00 +0200

    added LogItem

---
+++ Models/Api/LogItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ReportPortalNUnitLog4netClient.Models.Api
+{
+    public class LogItem
+    {
+        [JsonProperty("binaryContent")]
+        public BinaryContent BinaryContent { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("itemId")]
+        public string ItemId { get; set; }
+        [JsonProperty("launchId")]
+        public string LaunchId { get; set; }
+        [JsonProperty("level")]
+        public LogLevel? Level { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+        [JsonProperty("time")]
+        public DateTime Time { get; set; }
+        [JsonProperty("uuid")]
+        public string Uuid { get; set; }
+    }
+}
