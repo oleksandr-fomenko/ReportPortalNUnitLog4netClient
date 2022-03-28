8b32cf7f8fdbae22ab466228049393352c0d5aa1
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-28 12:44:52 +0300

    added filter api models

---
+++ Models/Api/Condition.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace ReportPortalNUnitLog4netClient.Models.Api
+{
+    public class Condition
+    {
+        [JsonProperty("filteringField")]
+        public string FilteringField { get; set; }
+        [JsonProperty("condition")]
+        public string ConditionCondition { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
