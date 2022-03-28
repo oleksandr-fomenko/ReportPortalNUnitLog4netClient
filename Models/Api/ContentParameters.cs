a837974770e7fcecddd79b8cb3096431a4dd187a
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-28 13:32:54 +0300

    widget models

---
+++ Models/Api/ContentParameters.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ReportPortalNUnitLog4netClient.Models.Api
+{
+    public class ContentParameters
+    {
+        [JsonProperty("contentFields")]
+        public List<string> ContentFields { get; set; }
+        [JsonProperty("itemsCount")]
+        public long ItemsCount { get; set; }
+        [JsonProperty("widgetOptions")]
+        public Dictionary<string, object> WidgetOptions { get; set; }
+    }
+}
---
+++ Models/Api/Widget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ReportPortalNUnitLog4netClient.Models.Api
+{
+    public class Widget
+    {
+        [JsonProperty("contentParameters")]
+        public ContentParameters ContentParameters { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("filterIds")]
+        public List<long> FilterIds { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("share")]
+        public bool Share { get; set; }
+        [JsonProperty("widgetType")]
+        public string WidgetType { get; set; }
+    }
+}
